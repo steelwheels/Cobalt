d1c98e21bca8c14b115492432607c432b54509ad
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2014-12-23 11:24:41 +0900

    build: Update project file

--- Source/Cobalt.h
+++ Source/Cobalt.h
@@ -1,11 +1,25 @@
-/**
- * @file	Cobalt.h
- * @brief	File to include all header files for Cobalt Framework
- * @par Copyright
- *   Copyright (C) 2014 Steel Wheels Project
- */
+//
+//  Cobalt.h
+//  Cobalt
+//
+//  Created by Tomoo Hamada on 2014/12/23.
+//  Copyright (c) 2014年 Steel Wheels Project. All rights reserved.
+//
 
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cobalt.
+FOUNDATION_EXPORT double CobaltVersionNumber;
+
+//! Project version string for Cobalt.
+FOUNDATION_EXPORT const unsigned char CobaltVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cobalt/PublicHeader.h>
+#import "CBForwarders.h"
 #import "CBCommandLine.h"
 #import "CBOption.h"
 #import "CBOptionDefinition.h"
 #import "CBArgument.h"
+
+
+
